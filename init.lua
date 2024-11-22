b7b3fb4f434b949af88b7eeeb100b83e3691dcba
kane8n <takumi.kaneda@zozo.com>
2024-11-22 16:15:01 +0900

    add treesitter overriding setting

--- init.lua
+++ init.lua
@@ -47,6 +47,30 @@ return {
       end
     },
     {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        -- list like portions of a table cannot be merged naturally and require the user to merge it manually
+        -- check to make sure the key exists
+        if not opts.ensure_installed then
+          opts.ensure_installed = {}
+        end
+        vim.list_extend(opts.ensure_installed, {
+          "go",
+          "gomod",
+          "gosum",
+          "gotmpl",
+          "html",
+          "java",
+          "jq",
+          "json",
+          "scala",
+          "sql",
+          "terraform",
+          "yaml"
+        })
+      end
+    },
+    {
       "rebelot/heirline.nvim",
       opts = function(_, opts)
         local status = require("astronvim.utils.status")
